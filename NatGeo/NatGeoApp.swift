5f735eba9b06027fa6a54b0d94042a9144404b56
Ranbeer Singh <Ranbeersinghcauhan@gmail.com>
2022-03-20 11:50:52 +0530

    Initial Commit

---
+++ NatGeo/NatGeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NatGeoApp.swift
+//  NatGeo
+//
+//  Created by RANBEER SINGH on 20/03/22.
+//
+
+import SwiftUI
+
+@main
+struct NatGeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
